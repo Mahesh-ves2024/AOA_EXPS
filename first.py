b376ca69c1a557cfb69b39148b96793158c45d1f
Mahesh-ves2024 <2024.mahesh.kumavat@ves.ac.in>
2026-04-25 12:34:12 +0530

    Implement Insertion and Selection Sort algorithms

--- first.py
+++ first.py
@@ -1,1 +1,32 @@
+# a) Insertion Sort
 
+arr = [5, 2, 9, 1, 7]
+
+for i in range(1, len(arr)):
+    key = arr[i]
+    j = i - 1
+
+    while j >= 0 and arr[j] > key:
+        arr[j + 1] = arr[j]
+        j = j - 1
+
+    arr[j + 1] = key
+
+print("Sorted array using Insertion Sort:", arr)
+
+
+
+# b) Selection Sort
+
+arr = [5, 2, 9, 1, 7]
+
+for i in range(len(arr)):
+    min_index = i
+
+    for j in range(i + 1, len(arr)):
+        if arr[j] < arr[min_index]:
+            min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print("Sorted array using Selection Sort:", arr)
