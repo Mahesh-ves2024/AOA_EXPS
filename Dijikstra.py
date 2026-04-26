bedcf4e6d0bd694030915560412fa0e99ac47137
Mahesh-ves2024 <2024.mahesh.kumavat@ves.ac.in>
2026-04-26 09:10:32 +0530

    Prims

---
+++ Dijikstra.py
@@ -0,0 +1,28 @@
+# 5 Prim Algorithm
+
+graph = [
+    [0, 2, 0, 6, 0],
+    [2, 0, 3, 8, 5],
+    [0, 3, 0, 0, 7],
+    [6, 8, 0, 0, 9],
+    [0, 5, 7, 9, 0]
+]
+
+n = len(graph)
+selected = [False] * n
+selected[0] = True
+
+for k in range(n - 1):
+    min_val = 9999
+
+    for i in range(n):
+        if selected[i]:
+            for j in range(n):
+                if not selected[j] and graph[i][j] > 0:
+                    if graph[i][j] < min_val:
+                        min_val = graph[i][j]
+                        x = i
+                        y = j
+
+    print(x, "-", y, "=", graph[x][y])
+    selected[y] = True
